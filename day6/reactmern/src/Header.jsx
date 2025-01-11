1a71cf178792129008285797ab24d6fbc7cb1d73
Sudharsanprakalathan VM <sudharsanprakalathanvm@gmail.com>
2025-01-11 12:34:35 +0530

    day6

---
+++ day6/reactmern/src/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+//JS block
+
+  return (
+    //HTML block
+
+    <div>Header
+
+    <h1>Hello all!..</h1>
+
+
+</div>
+
+  )
+}
+
+export default Header
